1c5724387f203f78857ed53a4224181d3cafe14c
pankaj <bhatrapankaj568@gmail.com>
2026-02-21 04:16:30 +0000

    simple calculate python code

---
+++ simple_calculator.py/calculator.py
@@ -0,0 +1,15 @@
+[200~simple_calculator.py
+  def add(x, y):
+      return x + y
+
+  def subtract(x, y):
+      return x - y
+
+  print("Simple Calculator")
+  num1 = float(input("Enter first number: "))
+  num2 = float(input("Enter second number: "))
+
+  print("Addition:", add(num1, num2))
+  print("Subtraction:", subtract(num1, num2))
+
+
